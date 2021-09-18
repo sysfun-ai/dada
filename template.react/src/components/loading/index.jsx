947e14b51a2a2561f1e9364c910f8c96aaed4ac9
panda <panda@smk.ai>
2021-09-19 05:45:18 +0800

    feat(loading): add loading component

---
+++ template.react/src/components/loading/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { styled } from '@linaria/react'
+
+// Loading 加载中
+export const Loading = () => {
+    return <Page>Loading...</Page>
+}
+
+const Page = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
