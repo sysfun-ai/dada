46a6342f645e1cb08aa9bf2562da326225c61eed
panda <panda@smk.ai>
2021-09-19 18:28:33 +0800

    feat(dada): use goreleaser to publish the binary

--- app/build.go
+++ app/build.go
@@ -4,3 +4,11 @@ var (
 	Version = ""
 	GitRev  = ""
 )
+
+func SetVersion(v string) {
+	Version = v
+}
+
+func SetGitRev(v string) {
+	GitRev = v
+}
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/sysfun-ai/dada/app/cmd"
+import (
+	"github.com/sysfun-ai/dada/app"
+	"github.com/sysfun-ai/dada/app/cmd"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+)
 
 func main() {
+	app.SetVersion(version)
+	app.SetGitRev(commit)
 	cmd.Execute()
 }
